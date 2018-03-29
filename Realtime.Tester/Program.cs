a6adcf6c8350a488f3ea85762ed7e6a01dabe994
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2018-03-28 20:04:35 -0700

    added a .net 4.6.1 tester

---
+++ Realtime.Tester/Program.cs
@@ -0,0 +1,51 @@
+﻿using Realtime.Tester.RabbitMq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Realtime.Tester.Mirth;
+
+namespace Realtime.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Realtime tester using .Net Framework 4.6.1");
+
+            //string mirthhostname = "fabricrealtimerabbitmq.eastus.cloudapp.azure.com";
+            string mirthhostname = "fabricrealtime.eastus.cloudapp.azure.com";
+
+            if (args.Length < 1)
+            {
+                // host was not passed on the command line
+                Console.WriteLine("Enter host to connect to:");
+                mirthhostname = Console.ReadLine();
+            }
+            else
+            {
+                mirthhostname = args[0];
+            }
+
+            Console.WriteLine($"Connecting to host: {mirthhostname}");
+
+            string rabbitmqhostname = mirthhostname;
+
+            RabbitMqTester.TestSecureConnectionToRabbitMq(rabbitmqhostname);
+
+            IRabbitMqListener rabbitMqListener = new RabbitMqListener();
+
+            MirthTester.TestSendingHL7(mirthhostname, rabbitMqListener);
+
+            //var rabbitMqListener = new RabbitMqListener();
+
+            //rabbitMqListener.StartListening(mirthhostname);
+
+            Console.WriteLine("Press Enter to exit");
+
+            Console.ReadLine();
+
+        }
+    }
+}
